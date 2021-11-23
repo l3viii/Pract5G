d2aa40c063762806b9800eea7b3e9a9a7f1a48be
g3teg <g3teg@DESKTOP-SIIESEU>
2021-11-23 10:22:58 +0300

    Добавьте файлы проекта.

---
+++ Practice_5G/Pair.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_5G
+{
+    class Pair
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+
+        public Pair(int first, int second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public Pair Multiply(Pair multiplication)
+        {
+           return new Pair(First * multiplication.First, Second * multiplication.Second);
+        }
+
+        public Pair Multiply()
+        {
+            return new Pair(First * 2, Second * 2); 
+        }
+    }
+}
